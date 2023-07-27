ddee96f18b8f0b51ff249d754969da0728193503
MinboChung <Minbo_chung@hotmail.com>
2023-07-27 21:05:43 +0200

    Added Create database function

--- mysql_bespoke.py
+++ mysql_bespoke.py
@@ -4,12 +4,27 @@
 #   File created: 2023-07-23                                     #
 ##################################################################
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from mysql import connector
 from mysql.connector import Error
 from my_credentials import MyCredentials
 
 class MySQLConnector(ABC):
+
+    sql_commands:dict = {
+        '': "SELECT",
+        '': "UPDATE",
+        '': 'DELETE',
+        '': 'INSERT INTO',
+        '': 'CREATE DATABASE',
+        '': 'ALTER DATABASE',
+        '': 'CREATE TABLE',
+        '': 'ALTER TABLE',
+        '': 'DROP TABLE',
+        '': 'CREATE INDEX',
+        '': 'DROP INDEX',
+    }
+
     def __init__(self, 
                  hst:str = None,
                  usr:str = None,
@@ -69,11 +84,26 @@ class MySQLConnector(ABC):
         # 3. Check if the db_name already exist if not create
         existence_check_query = f"SHOW DATABASES"
         my_cursor.execute(existence_check_query)
-        
-        for x in my_cursor:
-            print(x)
+        db_list = [x for x in my_cursor if x[0] == db_name]
+        if len(db_list) > 0:
+            print(f"CREATE TB: {db_name} already exist")
+            return
+        else:
+            print(f"CREATE TB: {db_name} does not exist")
 
-        # create_db_query = f"CREATE DATABASE {db_name}"
+        # 4. Create db
+        create_db_query = f"CREATE DATABASE {db_name}"
+        my_cursor.execute(create_db_query)
+
+        # 5. Check if the db is created
+        existence_check_query = f"SHOW DATABASES"
+        my_cursor.execute(existence_check_query)
+        db_list = [x for x in my_cursor if x[0] == db_name]
+        if len(db_list) > 0:
+            print(f"CREATE TB: {db_name} created")
+        else:
+            print(f"CREATE TB: {db_name} failed to be created")
+
 
     def delete_database(self, db_name:str=None):
         # 1. Check if the db_name is defined
@@ -91,28 +121,29 @@ class MySQLConnector(ABC):
         my_cursor.execute(existence_check_query)
 
         db_list = [x for x in my_cursor if x[0] == db_name]
-        if len(db_list) == 1:
-            print(f"{db_name} exists")
+        if len(db_list) >= 1:
+            print(f"DROP TB: {db_name} exists")
         else:
-            print(f"{db_name} does not exist")
+            print(f"DROP TB: {db_name} does not exist")
             return
 
         # 4. Delete db
-        delete_query = f"DROP DATABASE {db_name}"
-        my_cursor.execute(delete_query)
+        delete_db_query = f"DROP DATABASE {db_name}"
+        my_cursor.execute(delete_db_query)
 
         # 5. Check if the db is deleted
         my_cursor.execute(existence_check_query)
         db_list = [x for x in my_cursor if x[0] == db_name]
         if len(db_list) == 0:
-            print(f"{db_name} is removed")
+            print(f"DROP TB: {db_name} is removed")
         else:
-            print(f"{db_name} is not removed")
+            print(f"DROP TB: {db_name} is not removed")
 
 
 if __name__ == "__main__":
     my_db_connection = MySQLConnector()
     my_db_connection.connect()
-    # my_db_connection.delete_database(db_name="heavenms")
-    # my_db_connection.create_database(db_name="first_db")
+    my_db_connection.create_database(db_name="minbo_db")
+    my_db_connection.delete_database(db_name="minbo_db")
     my_db_connection.close_connection()
+    ...
